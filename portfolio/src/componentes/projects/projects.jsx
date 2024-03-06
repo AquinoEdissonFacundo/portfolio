50ce3eb8aa3f9e499eb89c3768a636721ce68637
EdissonFacundo <aquinoedissonfacundo@gmail.com>
2024-03-06 02:02:55 -0300

    config vite

--- portfolio/src/componentes/projects/projects.jsx
+++ portfolio/src/componentes/projects/projects.jsx
@@ -1,19 +1,85 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './projects.module.css';
-import { Fade } from 'react-reveal';
+import { motion } from 'framer-motion';
 
 const Projects = () => {
+  const projects = [
+    {
+      title: 'GLAMIFY E-COMMERCE',
+      description: 'APLICACIÓN DE MODA DE ALTA COSTURA...',
+      image: '/glamify.jpg',
+      githubLink: 'https://github.com/tu-usuario/proyecto1',
+      delay: 0.5,
+    },
+    {
+      title: 'AGILIX',
+      image: '/Agilix.png',
+      description: 'AGILIX: APLICACIÓN TODO EN UNO PARA PYMES...',
+      githubLink: 'https://github.com/tu-usuario/proyecto2',
+      delay: 0.7,
+    },
+    {
+      title: 'FOOD',
+      description: 'APLICACIÓN WEB PARA BUSCAR Y DESCUBRIR RECETAS...',
+      image: '/Food.png',
+      githubLink: 'https://github.com/tu-usuario/proyecto3',
+      delay: 0.9,
+    },
+  ];
+
+  const [visibleProjects, setVisibleProjects] = useState([]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const windowHeight = window.innerHeight;
+
+      const newVisibleProjects = projects.reduce((acc, project, index) => {
+        const element = document.getElementById(`project-${index}`);
+        if (element) {
+          const elementTop = element.offsetTop;
+          if (scrollPosition + windowHeight > elementTop) {
+            acc.push(index);
+          }
+        }
+        return acc;
+      }, []);
+
+      setVisibleProjects(newVisibleProjects);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [projects]);
+
   return (
     <div className={styles.container}>
-      <Fade bottom delay={500}>
-        <div className={styles.project}>
+      {projects.map((project, index) => (
+        <motion.div
+          key={index}
+          id={`project-${index}`}
+          className={`${styles.project} ${
+            index % 2 === 1 ? styles.reversecontainer : ''
+          }`}
+          initial={{ opacity: 0, y: 50 }}
+          animate={{
+            opacity: visibleProjects.includes(index) ? 1 : 0,
+            y: visibleProjects.includes(index) ? 0 : 50,
+          }}
+          transition={{ duration: 0.5, delay: project.delay }}
+          whileHover='visible'
+        >
           <div className={styles.imageContainer}>
-            <img src='/glamify.jpg' alt='Proyecto 1' className={styles.image} />
+            <img
+              src={project.image}
+              alt={`Proyecto ${index + 1}`}
+              className={styles.image}
+            />
             <div className={styles.overlay}>
-              <a
-                href='https://github.com/tu-usuario/proyecto1'
-                className={styles.githubLink}
-              >
+              <a href={project.githubLink} className={styles.githubLink}>
                 <img
                   className={styles.icongithub}
                   src='GitHub.svg'
@@ -23,70 +89,11 @@ const Projects = () => {
             </div>
           </div>
           <div className={styles.text}>
-            <h2 className={styles.h2text}>GLAMIFY E-COMMERCE</h2>
-            APLICACIÓN DE MODA DE ALTA COSTURA COMPITIENDO CON TIENDAS ONLINE.
-            OFRECE EXPERIENCIA DE USUARIO EXCEPCIONAL: NAVEGACIÓN FLUIDA, COMPRA
-            FÁCIL Y GUARDAR FAVORITOS. PERMITE EXPLORAR SECCIONES, AÑADIR AL
-            CARRITO Y GUARDAR PARA FUTURAS COMPRAS. ENFOCADA EN LA EXCELENCIA DE
-            LA EXPERIENCIA DE COMPRA DE MODA DE LUJO.
+            <h2 className={styles.h2text}>{project.title}</h2>
+            {project.description}
           </div>
-        </div>
-      </Fade>
-      <Fade bottom delay={1000}>
-        <div className={`${styles.project} ${styles.reversecontainer}`}>
-          {' '}
-          <div className={styles.text}>
-            <h2 className={styles.h2text}>AGILIX</h2>
-            AGILIX: APLICACIÓN TODO EN UNO PARA PYMES, GESTIONANDO INVENTARIO,
-            CLIENTES Y VENTAS. FACILITA PAGOS CON CÓDIGOS QR DE MERCADO PAGO,
-            CONTROLA STOCK, Y OFRECE ANÁLISIS DE VENTAS. SIMPLIFICA PROCESOS Y
-            TOMA DE DECISIONES ESTRATÉGICAS CON GRÁFICOS COMPARATIVOS DE VENTAS
-            MENSUALES. IDEAL PARA NEGOCIOS MEDIANOS Y PEQUEÑOS.
-          </div>
-          <div className={styles.imageContainer}>
-            <img src='/Agilix.png' alt='Proyecto 2' className={styles.image} />
-            <div className={styles.overlay}>
-              <a
-                href='https://github.com/tu-usuario/proyecto2'
-                className={styles.githubLink}
-              >
-                <img
-                  className={styles.icongithub}
-                  src='GitHub.svg'
-                  alt='github'
-                />
-              </a>
-            </div>
-          </div>
-        </div>
-      </Fade>
-      <Fade bottom delay={1000}>
-        <div className={styles.project}>
-          <div className={styles.imageContainer}>
-            <img src='/Food.png' alt='Proyecto 3' className={styles.image} />
-            <div className={styles.overlay}>
-              <a
-                href='https://github.com/tu-usuario/proyecto3'
-                className={styles.githubLink}
-              >
-                <img
-                  className={styles.icongithub}
-                  src='GitHub.svg'
-                  alt='github'
-                />
-              </a>
-            </div>
-          </div>
-          <div className={styles.text}>
-            <h2 className={styles.h2text}>FOOD</h2>
-            APLICACIÓN WEB PARA BUSCAR Y DESCUBRIR RECETAS DE COMIDA Y POSTRES.
-            CONECTA CON UNA API PARA FILTRAR POR TIPO, ORDEN ALFABÉTICO,
-            BÚSQUEDA POR NOMBRE Y PAGINACIÓN. PERMITE DESCUBRIR MILES DE
-            RECETAS, INCLUSO CREAR LAS PROPIAS. INTERFAZ INTUITIVA PARA
-            NAVEGACIÓN.
-          </div>
-        </div>
-      </Fade>
+        </motion.div>
+      ))}
     </div>
   );
 };
